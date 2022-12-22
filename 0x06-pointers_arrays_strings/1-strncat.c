538a7a934ca92bdab9bfe443d83f46ab52f4a139
YonasTatek <yonastatekcse1@gmail.com>
2022-12-22 09:08:15 +0300

    pointer array string part 2 fix 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,9 +1,9 @@
 /**
- * _strncat - a function that concatenates two strings
+ * _strncat - a function that concatenates two strings.
  *
  * @dest: pointer to destination input
  * @src: pointer to source input
- * @n: number of bytes from @src
+ * @n: most number of bytes from @src
  *
  * Return: @dest
 */
@@ -14,13 +14,17 @@ char *_strncat(char *dest, char *src, int n)
 
 	c = 0;
 
+	/*find size of dest array*/
 	while (dest[c])
 		c++;
 
-	
+	/**
+	 * src does not need to be null terminated
+	 * if it contains n or more bytes
+	*/
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[c + i] = src[i];
-
+	/*null terminate dest*/
 	dest[c + i] = '\0';
 
 	return (dest);
--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,5 +1,6 @@
 /**
- * reverse_array 
+ * reverse_array - a function that reverses the
+ *                content of an array of integers
  *
  * @a: pointer to int array
  * @n: is the number of elements to swap
